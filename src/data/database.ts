c356bc54c4a702ed622f59d649d6cb08ed820b0e
Keiver Pacheco <Krpachecop@gmail.com>
2024-08-05 21:39:58 -0400

    change who him for poriyects

---
+++ src/data/database.ts
@@ -0,0 +1,19 @@
+import type { StudiesType } from "../types/types";
+
+export const Studies = [
+  {
+    title: "Curso Profesional de Git y GitHub",
+    academy: "Platzi",
+    year: 2021,
+  },
+  {
+    title: "Mind wide open",
+    academy: "Cisco Networkin Academy",
+    year: 2018,
+  },
+  {
+    title: "TSU. Analisis y Diseño de Sistemas",
+    academy: " Universidad Nacional Experimental de La Fuerza Armada Nacional",
+    year: 2018,
+  },
+] as StudiesType[];
---
+++ src/types/types.ts
@@ -0,0 +1,5 @@
+export type StudiesType = {
+title: string,
+academy: string,
+year: number
+}
